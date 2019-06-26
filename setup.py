03c443e6ccbed9d6c3731802b63d47ef056084f4
Stefan Davis <stefan@turnkeylinux.org>
2019-06-26 02:04:37 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-fifobuffer',
+    version='1.0',
+    description='A fifobuffer string library',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-fifobuffer',
+    packages=['fifobuffer'])
